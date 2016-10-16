926494f1bdfabb147be8756bd82a7db76f5e2ef4
Rizalito Flestado <rizalito.flestado@thomsonreuters.com>
2016-10-16 13:02:49 +0400

    add story data and profile

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 var pg = require('pg');
+var async = require('async');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,8 +12,88 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  var loc = {};
+  async.parallel([
+    function(callback) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+          client.query('SELECT * FROM salesforce.contact LIMIT 5', function(err, result) {
+            done();
+            if (err) {
+              console.error(err);
+              callback(err);
+            }
+            else {
+              loc.contacts = result.rows;
+            }
+            callback();
+          });
+        });
+      },
+      function(callback) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+          client.query('SELECT * FROM salesforce.story__c LIMIT 5', function(err, result) {
+            done();
+            if (err) {
+              console.error(err);
+              callback(err);
+            }
+            else {
+              loc.stories = result.rows;
+            }
+            callback();
+          });
+        });
+      }
+  ], function(err) {
+    response.render('pages/index', { loc });
+      // results is now equals to: {one: 'abc\n', two: 'xyz\n'}
+  });
+
+});
+
+app.get('/story/:id', function(request, response) {
+  var loc = {};
+  var storyId = request.params.id;
+  async.parallel([
+    function(callback) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+          client.query({
+              text: 'SELECT * FROM salesforce.story__c where story_id__c= $1',
+              values: [storyId]
+            }, function(err, result) {
+            done();
+            if (err) {
+              console.error(err);
+              callback(err);
+            }
+            else {
+              loc.story = result.rows[0];
+            }
+            callback();
+          });
+        });
+      },
+      function(callback) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+          client.query('SELECT * FROM salesforce.story__c LIMIT 10', function(err, result) {
+            done();
+            if (err) {
+              console.error(err);
+              callback(err);
+            }
+            else {
+              loc.stories = result.rows;
+            }
+            callback();
+          });
+        });
+      }
+  ], function(err) {
+    response.render('pages/story', { loc });
+      // results is now equals to: {one: 'abc\n', two: 'xyz\n'}
+  });
 });
 
 app.get('/cool', function(request, response) {
