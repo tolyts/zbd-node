fd424f4d85108b3b138e34f85319ded3236a5423
Rizalito Flestado <rizalito.flestado@thomsonreuters.com>
2017-01-26 19:27:08 +0400

    Adding ElasticSearch

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var app = express();
 var pg = require('pg');
 var async = require('async');
 
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: process.env.ES_HOST,
+  log: 'trace'
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -50,6 +56,47 @@ app.get('/', function(request, response) {
     response.render('pages/index', { loc });
       // results is now equals to: {one: 'abc\n', two: 'xyz\n'}
   });
+
+});
+
+app.get('/company/:id', function(req, res) {
+  var companyId = req.params.id;
+  var loc = {};
+  client.get({
+    index: 'companies_index',
+    type: 'company',
+    id: companyId,
+    _sourceInclude: ['name','otherNames']
+  }, function (err, result) {
+    console.log(result);
+    if(result.hasOwnProperty('found') && result.found) {
+      console.log('found');
+      loc.company = result._source;
+      res.render('pages/company', loc );
+    }
+    // ...
+  });
+  // client.search({
+  //   index: 'companies_index',
+  //   type: 'company',
+  //   body: {
+  //     query: {
+  //       term: {
+  //         id: companyId
+  //       }
+  //     },
+  //     _source : {
+  //      includes : [ "name" ]
+  //     }
+  //   }
+  // }).then(function (resp) {
+  //     loc.company = resp.hits.hits[0]._source;
+  //     console.log(loc);
+  //     res.render('pages/company', loc );
+  // }, function (err) {
+  //   res.render('pages/company', loc );
+  //     console.trace(err.message);
+  // });
 
 });
 
